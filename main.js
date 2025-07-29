564f5ab85bac3d8317df514c2c4fb2089e83ee47
fabian <fabianisaimendez@gmail.com>
2025-07-29 04:09:09 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import { header } from './components/header/headerComponent.js';
+import { seccion1 } from './components/seccion1/seccion1Component.js';
+
+function seccion() {
+
+    let seccion = document.createElement("section");
+
+    //header
+    seccion.appendChild(header());
+
+    //seccion
+    seccion.appendChild(seccion1());
+
+    return seccion;
+}
