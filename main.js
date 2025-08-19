873c3350dfeea6fc68a9caca009ddefc90b9e890
fabianmendez513 <fabianisaimendez@gmail.com>
2025-08-19 12:55:02 -0600

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import { header } from "./components/header/headerComponent.js";
-import { seccion } from "./components/seccion1/seccion1Component.js";
-
-let app = document.getElementById("app");
-app.appendChild(header());
-app.appendChild(seccion());
