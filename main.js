6b718999aec9b921473cce317190de0d8aebd69e
fabian <fabianisaimendez@gmail.com>
2025-08-19 11:46:31 -0600

    Actualizar imports y agregar productos.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import { header } from "./components/header/headerComponent.js";
 import { seccion } from "./components/seccion1/seccion1Component.js";
+<<<<<<< HEAD
 import { productos } from "/database/productos.js";
 
+=======
+>>>>>>> 45e801f (Actualizar imports y agregar productos.js)
 
 let app = document.getElementById("app");
 app.appendChild(header());
