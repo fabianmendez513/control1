58c421bf6b90e306fe87458d4f92ef024677a4bd
fabianmendez513 <fabianisaimendez@gmail.com>
2025-08-19 18:13:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 import { header } from "./components/header/headerComponent.js";
 import { seccion } from "./components/seccion1/seccion1Component.js";
-<<<<<<< HEAD
 import { productos } from "/database/productos.js";
-
-=======
->>>>>>> 45e801f (Actualizar imports y agregar productos.js)
 
 let app = document.getElementById("app");
 app.appendChild(header());
