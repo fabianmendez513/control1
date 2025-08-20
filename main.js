eeb6cee477853a032a88e13bee998bf8fb0961c3
fabianmendez513 <fabianisaimendez@gmail.com>
2025-08-20 07:27:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { header } from "./components/header/headerComponent.js";
-import { seccion } from "./components/seccion1/seccion1Component.js";
+import { seccion } from "../components/seccion1/seccion1Component.js";
 
 let app = document.getElementById("app");
 app.appendChild(header());
